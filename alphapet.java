165dc1ff6e7bca4c216ac8e50004a9a022d1e745
elavarasin <elavarasi1596@gmail.com>
2016-09-01 02:25:17 -0700

    alphabet.java
    A

--- alphapet.java
+++ alphapet.java
@@ -1,13 +1,13 @@
-public class alphapet {
+public class alphabet {
 
 	public static void main(String[] args) {
 char ch='*';
 System.out.println("enter a char:"+ch);
 if(ch>='a' && ch<='z' || ch>='A' && ch<='Z'){
-	System.out.println("Alphapet char");
+	System.out.println("Alphabet char");
 }
 else
-	System.out.println("not alphapet");
+	System.out.println("not alphabet");
 	}
 
 }
