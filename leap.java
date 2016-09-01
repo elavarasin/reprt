9e4dcf3eb97e236469b2b583f12cd36f3372f66d
elavarasin <elavarasi1596@gmail.com>
2016-09-01 02:37:50 -0700

    leap.java

---
+++ leap.java
@@ -0,0 +1,10 @@
+public class leap{
+public 
+int year;
+Scanner in=new Scanner(System.in);
+year=in.nextInt();
+if(year%4==0)
+System.out.println("leap year");
+else
+System.out.println("not leap year");
+}
