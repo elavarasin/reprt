3dafd6ba599475c7cc11dde10980a6506c27d365
elavarasin <elavarasi1596@gmail.com>
2016-09-01 02:22:09 -0700

    alphapet.java
    A

---
+++ alphapet.java
@@ -0,0 +1,13 @@
+public class alphapet {
+
+	public static void main(String[] args) {
+char ch='*';
+System.out.println("enter a char:"+ch);
+if(ch>='a' && ch<='z' || ch>='A' && ch<='Z'){
+	System.out.println("Alphapet char");
+}
+else
+	System.out.println("not alphapet");
+	}
+
+}
